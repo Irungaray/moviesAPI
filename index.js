de716e9279488ccf90dddf9fcd17e6201761da4f
Irungaray <LionelIrungaray@GMail.com>
2020-07-29 23:01:21 -0300

    Cl33 Implementado Boom

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const app = express();
 const { config } = require('./config/index.js');
 const moviesApi = require('./routes/movies.js');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 app.use(express.json());
 
+// Rutas
 moviesApi(app);
+app.use(notFoundHandler);
 
+// Middlewares de manejo de errores
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
